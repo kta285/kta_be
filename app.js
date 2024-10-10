88e4dd334045e8bb9f109f5e40f8c6dd97e237af
KooTaehyeon <vcro12123@naver.com>
2024-10-10 19:26:54 +0900

    🎉		프로젝트 시작

---
+++ app.js
@@ -0,0 +1,21 @@
+//npm init ===>package.json
+//npm i express dotenv morgan --s
+//npm i nodemon -g
+const express = require('express');
+require('dotenv').config();
+const morgan = require('morgan');
+const cors = require('cors');
+
+const port = process.env.PORT || 3333;
+
+const app = express();
+//미들웨어 설정
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan('dev'));
+//cors 관련 설정 미들웨어 : npm i cors --s
+app.use(cors()); //모든 도메인 허용
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
