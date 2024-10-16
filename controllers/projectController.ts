320e00492425795cd11e8f6532ab38254ddf9f65
jennie <mikjk0530@gmail.com>
2024-10-16 16:58:18 +0900

    🐛fix 충돌 해결

--- controllers/projectController.ts
+++ controllers/projectController.ts
@@ -9,3 +9,46 @@ exports.getProjects = async (req: Request, res: Response) => {
     return res.status(500).json({ error: '문제가 발생했습니다' });
   }
 };
+
+exports.postProjects = async (req: Request, res: Response) => {
+  let { title, amount, category, content, startDate, endDate } = req.body.body;
+  if (!(startDate instanceof Date && endDate instanceof Date)) {
+    startDate = new Date(startDate);
+    endDate = new Date(endDate);
+  }
+  try {
+    const startDateFomat = startDate.toISOString().split('T')[0];
+    const endDateFomat = endDate.toISOString().split('T')[0];
+    await db.execute(
+      'INSERT INTO starfunding.Projects (title, description, goal_amount,start_date,end_date,type,status) VALUES (?,?,?,?,?,?,?)',
+      [
+        title,
+        content,
+        Number(amount),
+        startDateFomat,
+        endDateFomat,
+        category,
+        'pending',
+      ],
+    );
+    await db.query('COMMIT');
+    return res
+      .status(200)
+      .json({ message: '프로젝트가 성공적으로 작성되었습니다.' });
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({ error: '문제가 발생했습니다' });
+  }
+};
+
+exports.getProjectDetail = async (req: Request, res: Response) => {
+  const projectId = req.params.id;
+  const sql = `SELECT * FROM Projects WHERE project_id = ?`;
+  try {
+    const projectsDetail = await db.query(sql, [projectId]);
+    return res.status(200).json(projectsDetail[0]);
+  } catch (error) {
+    return res.status(500).json({ error: '문제가 발생했습니다' });
+  }
+};
--- router/project.ts
+++ router/project.ts
@@ -6,4 +6,73 @@ const multer = require('multer');
 const upload = multer({ storage: multer.memoryStorage() });
 router.get('/all', projectController.getProjects); // 전체 이미지 목록 조회
 
+/**
+ * @swagger
+ * /project/write:
+ *   post:
+ *     summary: 프로젝트 작성
+ *     description: 프로젝트 정보를 데이터베이스에 추가합니다.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               body:
+ *                 type: object
+ *                 properties:
+ *                   title:
+ *                     type: string
+ *                   content:
+ *                     type: string
+ *                   amount:
+ *                     type: string
+ *                   startDate:
+ *                     type: string
+ *                     format: date-time
+ *                   endDate:
+ *                     type: string
+ *                     format: date-time
+ *                   category:
+ *                     type: string
+ *                   status:
+ *                     type: string
+ *                 required:
+ *                   - title
+ *                   - content
+ *                   - amount
+ *                   - startDate
+ *                   - endDate
+ *                   - category
+ *                   - status
+ *     responses:
+ *       200:
+ *         description: 성공적인 응답
+ *       400:
+ *         description: 잘못된 요청
+ */
+router.post('/write', projectController.postProjects); // 작성
+/**
+ * @swagger
+ * /project/{id}:
+ *   get:
+ *     summary: 게시글 디테일
+ *     description: 해당 id에 맞는 게시글 데이터가 반환됨.
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: 게시글의 ID
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: 성공적인 응답
+ *       404:
+ *         description: 게시글을 찾을 수 없음
+ */
+router.get('/:id', projectController.getProjectDetail); // 게시글 디테일 가져오기
+module.exports = router;
+
 module.exports = router;
