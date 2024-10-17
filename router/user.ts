3bbde27d7dcd67d9d2975a3216040bce3aefafe0
jennie <mikjk0530@gmail.com>
2024-10-17 14:25:53 +0900

    🐛fix swagger 코드 추가

--- router/user.ts
+++ router/user.ts
@@ -42,8 +42,64 @@ const router = express.Router();
  *       500:
  *         description: 서버 오류
  */
-router.post('/signup', signupController.signup);
 
+/**
+ * @swagger
+ * /user/login:
+ *   post:
+ *     summary: 사용자 로그인
+ *     description: 사용자가 이메일과 비밀번호로 로그인합니다.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 format: email
+ *                 description: 사용자의 이메일 주소
+ *               password:
+ *                 type: string
+ *                 description: 사용자의 비밀번호
+ *             required:
+ *               - email
+ *               - password
+ *     responses:
+ *       200:
+ *         description: 로그인 성공
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: 로그인 성공
+ *       401:
+ *         description: 로그인 실패 (잘못된 이메일 또는 비밀번호)
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: 일치하는 사용자 없음
+ *       500:
+ *         description: 서버 오류
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: 로그인 실패
+ */
+
+router.post('/signup', signupController.signup);
 router.post('/login', loginController.login);
 
-module.exports = router;
+export default router;
