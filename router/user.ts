c3ce688eaeeffa3cbc5c146411985a2c42f11151
hoya8906 <darkkuho@nate.com>
2024-10-18 09:10:17 +0900

    ✨mypage userInfo Login user identify

--- router/user.ts
+++ router/user.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 const signupController = require('../controllers/signupController');
 const loginController = require('../controllers/loginController');
+const userController = require('../controllers/userController');
 
 const router = express.Router();
 
@@ -101,5 +102,57 @@ router.post('/signup', signupController.signup);
  */
 router.post('/login', loginController.login);
 
+/**
+ * @swagger
+ * /user/info:
+ *   get:
+ *     summary: 사용자 정보 조회
+ *     description: 사용자 ID를 헤더에 포함하여 요청하면 해당 사용자의 정보를 반환합니다.
+ *     parameters:
+ *       - in: header
+ *         name: user_id
+ *         required: true
+ *         description: 사용자의 고유 ID
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: 성공적으로 사용자 정보를 반환
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 user_id:
+ *                   type: string
+ *                   description: 사용자의 고유 ID
+ *                 name:
+ *                   type: string
+ *                   description: 사용자 이름
+ *                 email:
+ *                   type: string
+ *                   description: 사용자 이메일
+ *       401:
+ *         description: 인증 오류 또는 사용자 정보 없음
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: "DB: 사용자 정보 없음"
+ *       500:
+ *         description: 서버 오류
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "DB: 처리 오류"
+ */
+router.get('/info', userController.getUser)
 
 module.exports = router;
