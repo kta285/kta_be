9354aea60a06f3dc91f6dc71e240605c45757b9b
hoya8906 <darkkuho@nate.com>
2024-10-21 17:39:23 +0900

    ♻️firebase 연동

--- app.ts
+++ app.ts
@@ -5,20 +5,24 @@ const cors = require('cors');
 const swaggerUI = require('swagger-ui-express');
 const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerConfig = require('./swagger/config');
+
 const port: number = 3333;
 const main = require('./router/main');
 const project = require('./router/project');
 const user = require('./router/user');
 const admin = require('./router/admin');
-const app = express();
 const inquiries = require('./router/inquiries')
+
+const app = express();
 
 // 미들웨어 설정
 app.use(morgan('dev'));
 app.use(cors());
+
 // 요청 본문 크기 제한을 10MB로 설정 (필요에 따라 더 크게 조정 가능)
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+
 // Swagger 스펙 정의
 const swaggerDocs = swaggerJsDoc(swaggerConfig);
 
--- router/project.ts
+++ router/project.ts
@@ -6,13 +6,16 @@ router.get('/all', projectController.getProjects); // 전체 프로젝트 목록
 
 router.get('/:id', projectController.getProjectDetail); // 프로젝트 세부 정보 조회
 
-router.post('/write', projectController.postProjects); // 프로젝트 작성
+// router.post('/write', projectController.postProjects); // 프로젝트 작성
 
 router.put('/modify', projectController.putProjectModify); // 프로젝트 수정
 
-router.put('/:id/:status', projectController.modifyProjectStatus);
+router.put('/:id/:status', projectController.modifyProjectStatus); // 프로젝트 상태값 변경
 
 router.post('/support/:project_id', projectController.supportProject); // 펀딩
 
 router.delete('/:id', projectController.deleteProject); // 프로젝트 삭제
+
+router.post('/write', projectController.uploadThumbnail, projectController.postProjects);
+
 module.exports = router;
