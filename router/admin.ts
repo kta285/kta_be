bc14ce578bf8f7dd568e513d498352a2a12fe03d
KooTaehyeon <vcro12123@naver.com>
2024-10-21 14:17:22 +0900

    ✨	feat	admin api 추가

--- router/admin.ts
+++ router/admin.ts
@@ -3,5 +3,9 @@ const router = express.Router();
 const adminController = require('../controllers/adminController');
 
 router.get('/project/all', adminController.getProjects); // 전체 이미지 목록 조회
+router.put('/project', adminController.putProjectModify); // 전체 이미지 목록 조회
+router.delete('/project/:id', adminController.deleteAdminProject); // 전체 이미지 목록 조회
+router.post('/image', adminController.createImage); // 전체 이미지 목록 조회
+router.put('/image/:id', adminController.updateImage); // 전체 이미지 목록 조회
 
 module.exports = router;
